59ed466703b340bbcc2e5a1f8449506c901ecf09
david <simplesam8856@gmail.com>
2021-06-28 15:39:48 +1000

    amazing code

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 4567
+const express = require('express');
+const app = express();
+const port = 4567;
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('hello world'));
 
-app.get('/reset-password', (req, res) => res.send('reset password'))
+app.listen(port, () => console.log(`listening on port: ${port}`));
 
-app.listen(port, () => console.log(`listening on port: ${port}`))
+while (true) {
+  console.log('looping is fun');
+}
